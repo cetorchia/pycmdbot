0067542087fc379e81125adc34bb69f1e5bf7903
Carlos Torchia <ctorchia87@gmail.com>
2012-03-25 01:21:03 -0400

    Added shell script for server deployment
     - Put it on a crontab to run every hour

---
+++ pycmdbot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd ~/pycmdbot
+
+# Check if they are already running
+IS_CTORCHIA87="$(ps -e e | grep -P '\d python pycmdbot.py -um -c conf/ctorchia87.cfg' | wc -l)"
+IS_PYCMDBOT="$(ps -e e | grep -P '\d python pycmdbot.py -a -c conf/pycmdbot.cfg' | wc -l)"
+
+# Run the command bots
+
+DATE="$(date +%Y%m%d%H%M%S)"
+
+if [ $IS_CTORCHIA87 -eq 0 ]; then
+  python pycmdbot.py -um -c conf/ctorchia87.cfg -l log/ctorchia87.log$DATE > log/ctorchia87.out$DATE 2> log/ctorchia87.err$DATE &
+fi
+
+if [ $IS_PYCMDBOT -eq 0 ]; then
+  python pycmdbot.py -a -c conf/pycmdbot.cfg -l log/pycmdbot.log$DATE > log/pycmdbot.out$DATE 2> log/pycmdbot.err$DATE &
+fi
