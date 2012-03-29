1db19ec95642d16eb67388ac2b3c453342326c98
Carlos Torchia <ctorchia87@gmail.com>
2012-03-29 01:24:02 -0400

    Do not log error output in shell script

--- pycmdbot.sh
+++ pycmdbot.sh
@@ -10,9 +10,9 @@ IS_PYCMDBOT="$(ps -e e | grep -P '\d python pycmdbot.py -a -c conf/pycmdbot.cfg'
 DATE="$(date +%Y%m%d%H%M%S)"
 
 if [ $IS_CTORCHIA87 -eq 0 ]; then
-  python pycmdbot.py -um -c conf/ctorchia87.cfg -l log/ctorchia87.log$DATE > log/ctorchia87.out$DATE 2> log/ctorchia87.err$DATE &
+  python pycmdbot.py -um -c conf/ctorchia87.cfg -l log/ctorchia87.log$DATE 1> /dev/null 2> /dev/null &
 fi
 
 if [ $IS_PYCMDBOT -eq 0 ]; then
-  python pycmdbot.py -a -c conf/pycmdbot.cfg -l log/pycmdbot.log$DATE > log/pycmdbot.out$DATE 2> log/pycmdbot.err$DATE &
+  python pycmdbot.py -a -c conf/pycmdbot.cfg -l log/pycmdbot.log$DATE 1> /dev/null 2> /dev/null &
 fi
